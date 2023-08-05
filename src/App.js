2d6cfa1014044973a393902a5e851b22569dce72
girivivek45 <vivek.21ug2020@iiitranchi.ac.in>
2023-08-05 12:51:24 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Head from './Head';
 import Note from './Note';
+import Notes from './Notes'
+
+function createcard(item){
+  return < Note title ={item.title} content ={item.content} />;
+
+}
+
 function App() {
   return (
     <div>
       <Head/>
-      <Note/>
+      {Notes.map(createcard)}
     </div>
   );
 }
---
+++ src/Notes.js
@@ -0,0 +1,15 @@
+const Note = [
+    { title : "WEB D",
+    content : "MERN Stack or Django"
+    },
+    {
+        title: "APP D",
+        content: "Flutter , Kotlin , Java"
+    },
+    {
+        title: "DSA",
+        content: "Arrays,Linkedlist, stack, queues, trees"
+    }
+
+];
+export default Note;
