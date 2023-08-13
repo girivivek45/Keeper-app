242d26f3337d963a43c4990b8021c418bac3e056
girivivek45 <vivek.21ug2020@iiitranchi.ac.in>
2023-08-13 22:13:35 +0530

    final project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 import Head from './Head';
 import Note from './Note';
 import Notes from './Notes'
 import Hooks from './Hooks';
 import Form from './Form';
 import Todo from './Todo';
+import Createarea from './Createarea';
 
-function createcard(item){
-  return < Note title ={item.title} content ={item.content} />;
 
-}
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
   return (
     <div>
       <Head/>
       <Hooks/>
-      {Notes.map(createcard)}
+      <Createarea onAdd={addNote}/>
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      
       
       
     </div>
